5377f5f4918f85a85b05154e06b463bbdcc9bba2
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-07-23 21:07:32 -0300

    Leetcode: Add solution for Problem 1: Two Sum

---
+++ leetcode/problems/twoSum.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums []int
+	var target int
+
+	nums = []int{2, 7, 11, 15}
+	target = 9
+
+	fmt.Println(solve(nums, target))
+
+}
+
+func solve(nums []int, target int) []int {
+	var sum int
+	var twoSum []int
+
+	for i, n1 := range nums {
+		for j, n2 := range nums {
+			if i == j {
+				continue
+			}
+			sum = n1 + n2
+			if sum == target {
+				twoSum = []int{i, j}
+				return twoSum
+			}
+		}
+	}
+
+	return nil
+}
+
+// Problem: Two Sum
+// Time: 20m
