c2e01b1ab6d889c7e797dca0a4392a82b59e5d1d
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-07-27 23:57:20 -0300

    Add solution for Problem 3: Longest Substring Without Repeating Characters

---
+++ leetcode/problems/longest-substring-without-repeating-characters/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	maxLen := 0
+
+	for i := 0; i < len(s); i++ {
+		seen := make(map[byte]bool)
+		length := 0
+
+		for j := i; j < len(s); j++ {
+			if seen[s[j]] {
+				break
+			}
+			seen[s[j]] = true
+			length++
+		}
+
+		if length > maxLen {
+			maxLen = length
+		}
+	}
+
+	return maxLen
+}
